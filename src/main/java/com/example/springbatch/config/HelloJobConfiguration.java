de41ce185976f922069b578171f566547b55758d
hanbee <sonhanbi1002@gmail.com>
2022-08-14 23:15:45 +0900

    ExecutionContext 관련 테스트 진행

--- src/main/java/com/example/springbatch/config/HelloJobConfiguration.java
+++ src/main/java/com/example/springbatch/config/HelloJobConfiguration.java
@@ -1,56 +1,60 @@
 package com.example.springbatch.config;
 
-import com.example.springbatch.CustomTasklet;
+import com.example.springbatch.tasklet.*;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 @Configuration
 @RequiredArgsConstructor
-@Slf4j
 public class HelloJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
+
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(helloStep())
-                .next(byeStep())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .next(step3())
+                .next(step4())
                 .build();
     }
 
     @Bean
-    public Step helloStep() {
-        return stepBuilderFactory.get("helloStep")
-                .tasklet((contribution, chunkContext) -> {
-                    JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
-                    jobParameters.getString("name");
-                    jobParameters.getLong("seq");
-                    jobParameters.getDate("date");
-                    jobParameters.getDouble("age");
-
-                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
-
-                    log.info("===== Hello Spring Batch =====");
-                    return RepeatStatus.FINISHED;
-                })
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(executionContextTasklet1)
                 .build();
     }
 
     @Bean
-    public Step byeStep() {
-        return stepBuilderFactory.get("byeStep")
-                .tasklet(new CustomTasklet())
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(executionContextTasklet2)
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(executionContextTasklet3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(executionContextTasklet4)
                 .build();
     }
 }
--- src/main/java/com/example/springbatch/tasklet/CustomTasklet.java
+++ src/main/java/com/example/springbatch/tasklet/CustomTasklet.java
@@ -1,4 +1,4 @@
-package com.example.springbatch;
+package com.example.springbatch.tasklet;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
