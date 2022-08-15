84d0d4deeb3cb4a98c43d8b8d7cec7c581bd04c3
hanbee <sonhanbi1002@gmail.com>
2022-08-15 18:01:04 +0900

    validate 확인 테스트 (default)

--- src/main/java/com/example/springbatch/config/JobConfiguration.java
+++ src/main/java/com/example/springbatch/config/JobConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,7 +25,8 @@ public class JobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
-                .validator(new CustomJobParametersValidator())
+//                .validator(new CustomJobParametersValidator())
+                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
                 .build();
     }
 
